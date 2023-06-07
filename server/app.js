7a0639a312895e1287e9a1425658cd7dc3f5e9e8
unknown <Amiram.ab@gmail.com>
2023-06-07 18:59:16 +0300

    feat: node and express app created on server side

---
+++ server/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+export const app = express(); // Create our Express Application Object
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
