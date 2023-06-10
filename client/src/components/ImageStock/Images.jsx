5a2fc4f8725d085f8cba5beb47f680e7e40bacb3
unknown <Amiram.ab@gmail.com>
2023-06-10 12:12:03 +0300

    feat: now display only the first 9 elements from the array

--- client/src/components/ImageStock/Images.jsx
+++ client/src/components/ImageStock/Images.jsx
@@ -3,9 +3,10 @@ import { card } from './Image.style.js';
 import ImageCard from './ImageCard.jsx';
 
 const Images = ({ images }) => {
+  const firstNineImages = images.slice(0, 9);
   return (
     <>
-      {images.map((image) => (
+      {firstNineImages.map((image) => (
         <div css={card} key={image.id}>
           <ImageCard image={image} />
         </div>
