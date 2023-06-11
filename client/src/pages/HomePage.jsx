4566b79bb417751ed168ac8d95b08b54b73ae0da
unknown <Amiram.ab@gmail.com>
2023-06-11 11:06:20 +0300

    feat: add loading spinner for better user experience

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,19 +1,34 @@
 /** @jsxImportSource @emotion/react */
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import { ImageContext } from "../hooks/ImageContext.js";
-import { container, grid, header } from "./HomePage.style.js";
+import { container, grid, header, spinner } from "./HomePage.style.js";
 import Images from "../components/ImageStock/Images/Images.jsx";
 
 const HomePage = () => {
   const { images } = useContext(ImageContext);
-  console.log(images);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (images.length > 0) {
+      setIsLoading(false);
+    }
+  }, [images]);
+
   return (
     <div css={container}>
       <h1 css={header}>Stunning royalty-free images & royalty-free stock</h1>
-      <div css={grid}>
-        <Images images={images} />
-      </div>
+      {isLoading ? (
+        <div css={spinner}>
+          <div>
+            <span>Loading...</span>
+          </div>
+        </div>
+      ) : (
+        <div css={grid}>
+          <Images images={images} />
+        </div>
+      )}
     </div>
   );
 };
--- client/src/pages/HomePage.style.js
+++ client/src/pages/HomePage.style.js
@@ -1,4 +1,4 @@
-import { css } from '@emotion/react';
+import { css } from "@emotion/react";
 
 export const header = css`
   display: flex;
@@ -25,4 +25,30 @@ export const grid = css`
   grid-gap: 20px;
   width: 60%;
   justify-items: center;
+`;
+
+export const spinner = css`
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+  &:after {
+    content: " ";
+    display: block;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border-radius: 50%;
+    border: 6px solid #3498db;
+    border-color: #3498db transparent #3498db transparent;
+    animation: spinner 1.2s linear infinite;
+
+    @keyframes spinner {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+  }
 `;
